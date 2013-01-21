8978f8b3cf163dd8e619750b6ad8545b5395621c
stat30fbliss <robabby23@gmail.com>
2013-01-20 22:43:41 -0600

    Added conditional template

--- LendLib.js
+++ LendLib.js
@@ -1,6 +1,7 @@
 var lists = new Meteor.Collection("Lists");
 
 if (Meteor.isClient) {
+
 /*
   Template.hello.greeting = function () {
     return "my list.";
@@ -18,6 +19,32 @@ if (Meteor.isClient) {
   Template.categories.lists = function () {
     return lists.find({}, {sort: {Category: 1}});
   };
+  
+  // We are declaring the 'adding_category' flag
+  Session.set('adding_category', false);
+  
+  // This returns true if adding_category has been 
+  // assigned a value of true
+  Template.categories.new_cat = function () {
+    return Session.equals('adding_category', true);
+  }
+
+  Template.categories.events({
+    'click #btnNewCat': function (e, t) {
+      Session.set('adding_category', true);
+      Meteor.flush();
+      focusText(t.find('#add-category'));
+    }
+  });
+
+  ///// Generic Helper Functions /////
+
+  // This function pits our cursor where it needs to be
+  function focusText(i) {
+    i.focus();
+    i.select();
+  };
+
 }
 
 if (Meteor.isServer) {
