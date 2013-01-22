fee228e0e5816cf1be8734873da0d1876551512c
stat30fbliss <robabby23@gmail.com>
2013-01-22 13:30:26 -0600

    Added admin account with authorizations

--- LendLib.js
+++ LendLib.js
@@ -1,1 +1,20 @@
 var lists = new Meteor.Collection("Lists");
+
+/* Checks to see if the user making changes is an Admin */
+
+function adminUser(userId) {
+  var adminUser = Meteor.users.findOne({username:"admin"});
+  return (userId && adminUser && userId === adminUser._id);
+}
+
+lists.allow({
+  insert: function(userId, doc) {
+    return adminUSer(userId);
+  },
+  update: function(userId, docs, fields, modifier) {
+    return adminUser(userId);
+  },
+  remove: function(userId, docs) {
+    return adminUser(userId);
+  }
+});
