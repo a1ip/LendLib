ae34e7e7937d1c569eb880c72ed28e520d655718
stat30fbliss <robabby23@gmail.com>
2013-01-22 11:28:51 -0600

    Added client, server, and public directories

---
+++ server/LendLibServer.js
@@ -0,0 +1,10 @@
+Meteor.publish("Categories", function() {
+return lists.find({}, {fields:{Category:1}});
+});
+
+Meteor.publish("listdetails", function(category_id) {
+return lists.find({_id:category_id});
+});
+Meteor.startup(function () {
+// code to run on server at startup
+});
